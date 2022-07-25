5542963a81f6cd88b8a34e1bbc4d24757cefce57
sayaka tamura <goooodnight@yahoo.co.jp>
2022-07-25 19:21:56 +0900

    init

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,22 @@
+class ContactsController < ApplicationController
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path
+    else
+      render :new
+    end
+  end
+
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :content)#1つずつ指定しなくても、まとめて渡すことができます。
+    #Contact.create(name: params[:contact][:name], email: params[:contact][:email],content: params[:contact][:content])
+  end
+
+end
